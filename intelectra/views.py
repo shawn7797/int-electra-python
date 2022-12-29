ae383528dd9d9067371c90c6ddacdee00052f543
shawn7797 <shawn7797@gmail.com>
2022-12-29 12:21:43 +0530

    Added index view

---
+++ intelectra/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from datetime import datetime
+
+def index(request):
+    now = datetime.now()
+    html = f'''
+    <html>
+        <body>
+            <h1>Hello from Int Electra!</h1>
+            <p>The current time is { now }.</p>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
